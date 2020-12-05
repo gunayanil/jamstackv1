9d2f3cf07fd34b7207b38089eeadff2978f53bb1
gunayanil <gunayanilemre@gmail.com>
2020-12-04 23:19:53 -0500

    changed main title

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          What a website for $10!
         </h1>
 
         <p className={styles.description}>
